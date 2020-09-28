1a19160f30643d4d1a6619259bc9e4bc26e0ef9b
eulores <43118918+eulores@users.noreply.github.com>
2020-09-28 19:46:10 +0200

    Fix spelling in package.json (3)

--- scripts/npmhelper.js
+++ scripts/npmhelper.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const {buildSync} = require('esbuild')
 
 function bundleSources() {
-  console.log('start bundling esimba sources');
   buildSync({
     bundle:true,
     entryPoints: ['../src/index.js'],
@@ -17,22 +16,19 @@ function bundleSources() {
     loader:{'.jst':'text'},
     external:['imba', 'esbuild', 'lru-cache', 'koa', 'koa-static-resolver', 'koa-easy-ws', 'chokidar', 'esc-exit', 'open', 'commander'],
   });
-  console.log('end bundling esimba sources');
 }
 
 function patchVersion() {
-  console.log('Version');
-  const {version, name} = require('./package.json');
+  const {version, name} = require('../package.json');
   const pkg = JSON.parse(fs.readFileSync(`./pkg/package.json`, 'utf8'));
-  fs.writeFileSync(`./pkg/package.json`, JSON.stringify({...pkg, version}, null, 2));
+  fs.writeFileSync(`./pkg/package.json`, JSON.stringify({...pkg, version, name}, null, 2));
 }
 
 function copyFiles() {
-  fs.copyFileSync('../README.md', './README.md');
+  fs.copyFileSync('./README.md', './pkg/README.md');
 }
 
 function cleanupFiles() {
-  console.log('Cleaning up...');
   fs.unlinkSync('./README.md');
   fs.unlinkSync('./esimba.js');
 }
@@ -41,8 +37,6 @@ console.log(`Doing ${process.env.npm_lifecycle_event} inside ${process.cwd()}`);
 switch(process.env.npm_lifecycle_event) {
   case 'version': patchVersion(); break;
   case 'build': bundleSources(); copyFiles(); break;
-  // case 'build': console.log('Building'); break;
-  // case 'prepublishOnly': console.log('prepublishOnly'); break;
   default:
     console.log('This script got called at a different lifecycle event:', process.env.npm_lifecycle_event);
 }
